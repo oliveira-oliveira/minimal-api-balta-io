33f72fd9d6031fcd5f4215cf77e5b50513e01edf
Diego Oliveira <diego.oliveira@grupocheck.com.br>
2023-11-08 11:30:19 -0300

    Conclusão básica da minimal api

--- MinhaApi/Program.cs
+++ MinhaApi/Program.cs
@@ -3,4 +3,25 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/nome={nome}", (string nome) => 
+{
+    return Results.Ok($"Olá, {nome}");
+});
+
+app.MapGet("/nome/{nome}", (string nome) => 
+{
+    return Results.Ok($"Olá, {nome}");
+});
+
+app.MapPost("", (Usuario usuario) => 
+{
+    return Results.Ok(usuario);
+});
+
 app.Run();
+
+public class Usuario
+{    
+    public int Id { get; set; }
+    public string Nome { get; set; }
+}
