55012ed0d2f0e2d1b8213aeddca82e1072ad4471
Diego Oliveira <diego.oliveira@grupocheck.com.br>
2023-11-10 22:57:40 -0300

    remoção de comentários

--- MinhaApi/Program.cs
+++ MinhaApi/Program.cs
@@ -4,34 +4,4 @@ builder.Services.AddControllers();
 var app = builder.Build();
 app.MapControllers();
 
-/*
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/nome={nome}", (string nome) => 
-{
-    return Results.Ok($"Olá, {nome}");
-});
-
-List<Usuario> listaUsuarios = new List<Usuario>();
-
-app.MapGet("/usuario/", () => 
-{
-    return Results.Ok(listaUsuarios);
-});
-
-app.MapPost("/", (Usuario usuario) => 
-{
-    usuario.Id = listaUsuarios.Count + 1;
-    listaUsuarios.Add(usuario);
-
-    return Results.Ok(usuario);
-});
-*/
-
 app.Run();
-
-// public class Usuario
-// {
-//     public int Id { get; set; }
-//     public string Nome { get; set; }
-// }
